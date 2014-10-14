e038a21c29eb3dafc0d13f6f91e264db895b6fcb
shdshdw <ronald_hey_@hotmail.com>
2014-10-14 15:51:47 +0200

    getters and setters for ant object

--- CI_Group/src/com/AntColony/Group46/Ant.java
+++ CI_Group/src/com/AntColony/Group46/Ant.java
@@ -5,7 +5,23 @@ public class Ant {
 	private int y;
 	
 	public Ant(int x, int y) {
-		this.x = x;
+		this.setX(x);
+		this.setY(y);
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
 		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
 	}
 }
